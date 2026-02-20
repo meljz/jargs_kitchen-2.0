116ce0ecc9e86dda611151b51687994a3b323b0c
Jay <reyesmeljohnjay@gmail.com>
2026-02-20 15:04:18 +0800

    Remove unused files

--- src/app/slide-in-on-scroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SlideInOnScrollDirective } from './slide-in-on-scroll.directive';
-
-describe('SlideInOnScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SlideInOnScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
